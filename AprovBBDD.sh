ee7f7ca67088f966500acea4a11d205c3b658568
drodriguezg28 <drodriguezg28@iesalbarregas.es>
2025-12-02 23:32:03 +0100

    Update AprovBBDD.sh

--- AprovBBDD.sh
+++ AprovBBDD.sh
@@ -3,7 +3,7 @@
 # Instalación de mariadb-server
 sudo apt update
 sudo apt install mariadb-server -y
-echo "MariaDB se ha instalado correctamente y está activo."
+echo "MariaDB se ha instalado correctamente y estÃ¡ activo."
 
 mysql -u root <<MYSQL_SCRIPT
 CREATE DATABASE wordpress;
@@ -12,5 +12,12 @@ GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
+sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo systemctl restart mariadb
+echo "MariaDB permite conexones."
+
+
 sudo hostnamectl set-hostname DanielRodriguez-BBDD
+echo "127.0.1.1   DanielRodriguez-BBDD" | sudo tee -a /etc/hosts
 echo "Nombre del host cambiado a DanielRodriguez-BBDD."
+
