9c053dfa1657128f19e22d5e7fd52ab5d860a3c7
drodriguezg28 <drodriguezg28@iesalbarregas.es>
2025-12-02 23:32:56 +0100

    Update AprovNFS.sh

--- AprovNFS.sh
+++ AprovNFS.sh
@@ -2,54 +2,39 @@
 
 # Instalación de NFS
 sudo apt update
-sudo apt install apache2 -y
 sudo apt install nfs-kernel-server -y
 echo "NFS se ha instalado correctamente y está activo."
 
 # Preparar el entorno para WordPress
-sudo a2enmod rewrite
-sudo mkdir -p /var/www/wordpress
+sudo mkdir -p /var/www/html
 
 # Descargar y descomprimir WordPress
-cd /var/www/wordpress
+cd /var/www/html
 sudo wget https://wordpress.org/latest.tar.gz
 sudo tar -xzf latest.tar.gz
 sudo mv wordpress/* .
 sudo rm -rf wordpress
 sudo rm -f latest.tar.gz
-echo "WordPress se ha descargado y descomprimido en /var/www/wordpress."
+echo "WordPress se ha descargado y descomprimido en /var/www/html."
 
 # volver al directorio principal
 cd ~
 
 # Configurar permisos para WordPress
-sudo chown -R www-data:www-data /var/www/wordpress
-sudo find /var/www/wordpress -type d -exec chmod 775 {} \;
-sudo find /var/www/wordpress -type f -exec chmod 664 {} \;
+sudo chown -R www-data:www-data /var/www/html
+sudo find /var/www/html -type d -exec chmod 775 {} +
+sudo find /var/www/html -type f -exec chmod 664 {} +
 echo "Permisos de WordPress configurados."
 
 #configurar NFS para compartir el directorio /var/www/html
-echo "/var/www/wordpress    192.168.10.21(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
-echo "/var/www/wordpress    192.168.10.22(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/var/www/html    192.168.10.20(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/var/www/html    192.168.10.21(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
-echo "NFS se ha configurado para compartir el directorio /var/www/wordpress."
+echo "NFS se ha configurado para compartir el directorio /var/www/html."
 
-# Configurar Apache para servir WordPress
-sudo a2enmod ssl 
-cd /etc/apache2/sites-available/
-cp 000-default.conf wordpress.conf
-sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/wordpress|' wordpress.conf
-sudo sed -i '/<\/VirtualHost>/i \
-<Directory /var/www/wordpress>\n\
-    AllowOverride All\n\
-    Require all granted\n\
-</Directory>' wordpress.conf
 
-sudo a2ensite wordpress.conf
-sudo a2dissite 000-default.conf
-sudo systemctl restart apache2
-echo "Apache configurado para servir WordPress desde /var/www/wordpress."
-
+# Indicar el nuevo nombre del host
 sudo hostnamectl set-hostname DanielRodriguez-NFS
+echo "127.0.1.1   DanielRodriguez-NFS" | sudo tee -a /etc/hosts
 echo "Nombre del host cambiado a DanielRodriguez-NFS."
